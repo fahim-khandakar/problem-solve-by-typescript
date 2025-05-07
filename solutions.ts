966f24cb42a3799685ffc5c28fe201513eb3c27b
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-05-07 22:04:59 +0600

    sixth problem solve

--- solutions.ts
+++ solutions.ts
@@ -69,3 +69,24 @@ const processValue = (value: string | number): number => {
 
 // console.log(processValue("hello"));
 // console.log(processValue(10));
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+const getMostExpensiveProduct = (products: Product[]): Product | null => {
+  if (products.length === 0) return null;
+  return products.reduce((prev, curr) =>
+    prev.price > curr.price ? prev : curr
+  );
+};
+
+// const products = [
+//     { name: "Pen", price: 10 },
+//     { name: "Notebook", price: 25 },
+//     { name: "Bag", price: 50 },
+// ];
+
+// console.log(getMostExpensiveProduct(products));
+// Output: { name: "Bag", price: 50 }

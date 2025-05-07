3077d66ff2a199564ad5de81d483179b9ae5ebbb
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-05-07 21:07:34 +0600

    second problem solve

--- solutions.ts
+++ solutions.ts
@@ -4,3 +4,21 @@ const formatString = (input: string, toUpper: boolean = true): string => {
   }
   return input?.toLocaleLowerCase();
 };
+
+// console.log(formatString("Hello World"));
+// console.log(formatString("Hello World", false));
+// console.log(formatString("Hello World", true));
+
+const filterByRating = (
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] => {
+  return items?.filter((item) => item?.rating >= 4); // Example: filter items with rating >= 4
+};
+
+// const books = [
+//   { title: "Book A", rating: 3.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 3.0 },
+// ];
+
+// console.log(filterByRating(books));
